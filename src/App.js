94b44f470dca934fedfc29ad6098a62b5877f2a5
Michelle Opey <mipanowicz@gmail.com>
2020-10-15 16:49:31 +0200

    added db-file & small render to see if it works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import Event from './Event.js'
 
 export default function App() {
+
+  const [events, setEvents] = useState([]);
+
+  async function fetchEvents() {
+    const events = (await (await fetch("api/event")).json());
+    setEvents(events);
+    console.log('har hämtat events ', events);
+  }
+
+  //kör när det startas, bara en gång
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
   return (
-    <div>
-      <h3>Hello world!</h3>
-      <p>This is my first React app!</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          {/* 
+          //Add your routes and component names here!
+          <Route exact path="/" component={} />
+          <Route exact path="/invitations" component={} />
+          <Route path="/event" component={} />
+          <Route path="/event/:id" component={} /> 
+        */}
+        </Switch>
+
+        <div className="container">
+          <div className="row mt-5">
+            <div className="col-12 w-100">
+              <h1>HellO!</h1>
+              <h3>check to see if your database work..</h3>
+              <div className="row mt-5">
+              <h2>Events:</h2>
+                <div className="col-12">
+                  {events.map((event) => {
+                  return <Event key={event.id} {...event} />;
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
