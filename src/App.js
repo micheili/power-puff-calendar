ab989ba463abb3556b6ffdf47682d1fec4fb70b4
anna <zoora1234@hotmail.com>
2020-10-16 13:37:00 +0200

    working calendar
    a functioning calendar, only "days" done, still have to do "weeks, and "months".
    Ui not done.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import CalendarView from './CalendarView';
+import CalendarView from './calendar/CalendarView';
 
 export default function App() {
   return (
 
     <Router>
     <div>
-           
+           <div className="container">
       <Route exact path="/"  component={CalendarView} />
+      </div>
 
     </div>
     </Router>
---
+++ src/calendar/BuildCalendar.js
@@ -0,0 +1,22 @@
+import { calendarFormat } from "moment";
+
+export default function BuildCalendar(value){
+    
+      //startday for month
+      const startDay = value.clone().startOf('month').startOf('week');
+      //endDay of month
+      const endDay = value.clone().endOf('month').endOf('week');
+
+            //iterator, get all dates between startday and endday
+       //containing weeks and weeks containing days
+     const day = startDay.clone().subtract(1, 'day');
+     const calendar = [];
+     //isbefore = method from moment, (endday is when the loop stops, day is the interval)
+     while(day.isBefore(endDay, 'day')){
+         calendar.push(
+             Array(7).fill(0).map(() => day.add(1, 'day').clone())
+         );
+     }
+
+     return calendar;
+}
