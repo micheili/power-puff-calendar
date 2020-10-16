090497d9a93b07837bc8568ad4651935d93cefff
Sofia <sofia.fredmans@gmail.com>
2020-10-16 09:26:36 +0200

    refactor

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,23 +3,27 @@ import { Link } from 'react-router-dom';
 
 export default function Login(){
     return (
-        <div className="container-fluid mt-5">
+        <div className="homePageContainer container-fluid mt-5">
             <div className="row justify-content-center">
                 <form>
+                <h3 className="row justify-content-center mb-5">Welcome back</h3>
                     <section className="col-12">
                         <div className="form-group">
-                            <label htmlFor="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
+                            <label>Email address
+                                <input name="email" type="email" className="form-control" aria-describedby="emailHelp" required/>
+                            </label>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="exampleInputPassword1">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" required/>
+                            <label>Password
+                                <input name="password" type="password" className="form-control" required/>
+                            </label>
                         </div>
                         <Link to="/Register"><p className="row justify-content-center">Don't have an account?</p></Link>
-                        <button type="submit" className="btn btn-primary btn-block">Sign in</button>
+                        <button type="submit" className="btn btn-primary btn-block">Login</button>
                     </section>
                 </form>
             </div>
-        </div>       
+        </div>
+               
     ) 
 } 
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,17 +3,25 @@ import { Link } from 'react-router-dom';
 
 export default function Register(){
     return (
-        <div className="container-fluid mt-5">
+        <div className="homePageContainer container-fluid mt-5">
             <div className="row justify-content-center">
                 <form>
+                <h3 className="row justify-content-center mb-5">Create account</h3>
                     <section className="col-12">
-                        <div className="form-group">
-                            <label htmlFor="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control"  aria-describedby="emailHelp" required/>
+                    <div className="form-group">
+                            <label>Username
+                                <input name="username" type="text" className="form-control" required/>
+                            </label>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="exampleInputPassword1">Password</label>
-                            <input type="password" className="form-control" required />
+                            <label>Email address
+                                <input name="email" type="email" className="form-control"  aria-describedby="emailHelp" required/>
+                            </label>
+                        </div>
+                        <div className="form-group">
+                            <label>Password
+                                <input name="password" type="password" className="form-control" required />
+                            </label>
                         </div>
                         <Link to="/"><p className="row justify-content-center">Already have an account?</p></Link>
                         <button type="submit" className="btn btn-primary btn-block">Sign up</button>
