6419f301954524f9f21d6bd48c28cb1ba4602357
anna <zoora1234@hotmail.com>
2020-10-16 11:19:09 +0200

    Update CalendarView.js
    Now we can se current month and its days in an
    provisionally UI

--- src/CalendarView.js
+++ src/CalendarView.js
@@ -15,7 +15,7 @@ export default function CalendarView(){
     //iterator, get all dates between startday and endday
     //containing weeks and weeks containing days
     const day = startDay.clone().subtract(1, 'day');
-    const calender = [];
+    const calendar = [];
      //isbefore = method from moment, (endday is when the loop stops, day is the interval)
 
      while(day.isBefore(endDay, 'day')){
@@ -31,38 +31,46 @@ export default function CalendarView(){
  <h1 className="display-4">
  {day.format('MM/DD')}</h1>
 
- 
-   
+  
   <a className="btn btn-primary btn-lg" href="/" role="button">Days</a>
   <a className="btn btn-primary btn-lg" href="/weeks" role="button">Weeks</a>
   <a className="btn btn-primary btn-lg" href="/months" role="button">Months</a>
 
-    {calender.map((week) =>
-        <div>
-            {week.map((day) =>
-            <div>{day.format('D').toString()}</div>
-            )}
-        </div>
-    )}
+  </div>
 
-
+  <div class="table-responsive">
 <table className="table mt-4">
   <thead className="thead-dark">
     <tr>
-    <th scope="col"></th>
 
-     
-
+    <th scope="col">sunday</th>
+      <th scope="col">Monday</th>
+      <th scope="col">onsdag</th>
+      <th scope="col">torsdag</th>
+      <th scope="col">fredag</th>
+      <th scope="col">lördag</th>
+      <th scope="col">söndag</th>
+      
     </tr>
     
   </thead>
 
   <tbody>
-    <tr>
-    <th scope="col"></th>
   
-     
-    </tr>
+  
+  
+    {calendar.map((week) =>
+        <tr>
+            {week.map((day) =>
+             <td scope="col">{day.format('D').toString()}</td>
+            )}
+       </tr>
+    )}
+
+
+
+
+   
     
   </tbody>
 </table>
