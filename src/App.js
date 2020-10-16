7687cef0e3b7ee539e7cad27a2d1919b30c67c91
Michelle Opey <mipanowicz@gmail.com>
2020-10-16 09:36:04 +0200

    something old was still in app

--- src/App.js
+++ src/App.js
@@ -5,19 +5,6 @@ import Register from "./components/Register"
 
 export default function App() {
 
-  const [events, setEvents] = useState([]);
-
-  async function fetchEvents() {
-    const events = (await (await fetch("api/event")).json());
-    setEvents(events);
-    console.log('har hämtat events ', events);
-  }
-
-  //kör när det startas, bara en gång
-  useEffect(() => {
-    fetchEvents();
-  }, []);
-
   return (
     <Router>
       <div className="App">
