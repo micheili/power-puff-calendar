6d6e953699e28664bfe470afe454fd1b5133ea8f
anna <zoora1234@hotmail.com>
2020-10-20 10:36:13 +0200

    Update CalendarMonth.js
    Changed bootstrap

--- src/calendar/CalendarMonth.js
+++ src/calendar/CalendarMonth.js
@@ -8,29 +8,29 @@ export default function CalendarMonth() {
 
   const [calendar, setCalendar] = useState([]);
   const [value, setValue] = useState(moment());
+  
 
-  //startday for month
   const startDay = value.clone().startOf('month').startOf('week');
-  //endDay of month
   const endDay = value.clone().endOf('month').endOf('week');
 
-        //iterator, get all dates between startday and endday
-   //containing weeks and weeks containing days
- const day = startDay.clone().subtract(0, 'day');
- const ca = [];
- //isbefore = method from moment, (endday is when the loop stops, day is the interval)
- while(day.isBefore(endDay, 'day')){
-     ca.push(
-         Array(7).fill(0).map(() => day.add(1, 'day').clone())
-     );
- }
 
+  //iterator, get all dates between startday and endday
+  //containing weeks and weeks containing days
+  const day = startDay.clone().subtract(0, 'day');
+  const ca = [];
+  //isbefore = method from moment, (endday is when the loop stops, day is the interval)
+  while (day.isBefore(endDay, 'day')) {
+    ca.push(
+      Array(7).fill(0).map(() => day.add(1, 'day').clone())
+    );
 
-  
+  }
+
 
   useEffect(() => {
     setCalendar(ca);
   }, [value]);
+
 
   function currentMonthName() {
     return value.format('MMMM');
@@ -50,70 +50,73 @@ export default function CalendarMonth() {
 
 
 
-
   return (
     <div>
-    
-    <div className="container">
-      <div className="row justify-content-md-center bg-secondary">
-        <div className="col text-center">
-          <a className="btn btn-primary btn-lg btn-block my-2" href="/" role="button">Days</a>
+
+      <div className="container -sm">
+        <div className="row justify-content-md-center bg-secondary">
+          <div className="col text-center">
+            <a className="btn btn-primary btn-lg btn-block my-2" href="/" role="button">Days</a>
+          </div>
+          <div className="col text-center">
+            <a className="btn btn-primary btn-lg btn-block my-2" href="#" role="button">Weeks</a>
+          </div>
+          <div className="col text-center">
+            <a className="btn btn-primary btn-lg btn-block my-2" href="#" role="button">Months</a>
+          </div>
         </div>
-        <div className="col text-center">
-          <a className="btn btn-primary btn-lg btn-block my-2" href="#" role="button">Weeks</a>
-        </div>
-        <div className="col text-center">
-          <a className="btn btn-primary btn-lg btn-block my-2" href="#" role="button">Months</a>
-        </div>
-      </div>
       </div>
 
-      <div className="table-responsive">
-        <div className="border border-dark">
-          <div className="container ">
-            <div className="row">
-              <div className="previous col"
-                onClick={() => setValue(prevMonth())}>
-                {String.fromCharCode(171)}</div>
-              <h2 className="current col text-center font-weight-bolder ">
-                {currentMonthName()} {currentYear()}
-              </h2>
-              <div className="next col ">
-                <div className="float-right" onClick={() => setValue(nextMonth())}>
-                  {String.fromCharCode(187)}
+      <div className="container -sm">
+        <div className="row border border-dark bg-dark text-white">
+          <div className="col mt-2 mb-2">
+            Week
+    </div>
+          <div className="col mt-2 mb-2">
+            Monday
+    </div>
+          <div className="col mt-2 mb-2">
+            Tuesday
+    </div>
+          <div className="col mt-2 mb-2">
+            Wednesday
+    </div>
+          <div className="col mt-2 mb-2">
+            Thursday
+    </div>
+          <div className="col mt-2 mb-2">
+            Friday
+    </div>
+          <div className="col mt-2 mb-2">
+            Saturday
+    </div>
+          <div className="col mt-2 mb-2">
+            Sunday
+    </div>
+        </div>
+
+
+        {calendar.map((week) => (
+
+          <div className="row">
+
+            <div className="col mt-2 mb-2">
+              {value.format('WW').toString()}
+            </div>
+
+            {week.map((day) => (
+              <div className='day col border border-dark' onClick={() => setValue(day)}>
+                <div className={value.isSame(day, 'day') ? 'table-primary' : ''}>
+
+                  {day.format('D').toString()}
                 </div>
               </div>
-            </div>
+            ))}
           </div>
-          <table className="table mt-4">
-            <thead className="thead-dark">
-              <tr>
-                <th scope="col">Monday</th>
-                <th scope="col">Tuesday</th>
-                <th scope="col">Wednesday</th>
-                <th scope="col">Thursday</th>
-                <th scope="col">Friday</th>
-                <th scope="col">Saturday</th>
-                <th scope="col">Sunday</th>
-              </tr>
-            </thead>
-            <tbody>
-              {calendar.map((week) => (
-                <tr >
-                  {week.map((day) => (
-                    <td className='day' scope="col"  onClick={() => setValue(day)}>
-                      <div className={value.isSame(day, 'day') ? 'table-primary' : ''}>         
-                        {day.format('D').toString()}                   
-                      </div>                    
-                    </td>
-                  ))}               
-                </tr>
-              ))}
-                
-            </tbody>
-          </table>
-        </div>
+        ))}
+
       </div>
+
     </div>
   );
 
