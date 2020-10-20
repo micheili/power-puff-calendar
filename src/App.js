f2882bf6a6f07b71c6578147739d472ddcece394
Michelle Opey <mipanowicz@gmail.com>
2020-10-20 22:42:17 +0200

    routing just to see ugly box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import infobox from './components/InfoBox';
 import Sidebar from "./components/Sidebar";
 import Content from "./components/Content";
 import "./sass/style.scss";
@@ -15,6 +15,7 @@ const App = () => {
         <Sidebar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
         <Content toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen} />
       </div>
+      <Route path="/infobox"></Route>
     </Router>
   );
 };
