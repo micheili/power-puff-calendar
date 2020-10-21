3e82d61882a4e3189fd026f63367657467d8e9d3
Michelle Opey <mipanowicz@gmail.com>
2020-10-21 16:23:47 +0200

    liite finare styling

--- src/CalendarPage.js
+++ src/CalendarPage.js
@@ -6,12 +6,12 @@ import CalenderView from './calendar/CalendarView'
 export default function CalendarPage() {
   
   return (
-    <Container>
+    <Container fluid={true}>
       <Row>
-        <Col sm="12" lg="8">
+        <Col sm="12 mt-2" lg="8">
           <CalenderView /> 
         </Col>
-        <Col sm="12" lg="4">
+        <Col sm="12 mt-2" lg="4">
           <Infobox />
         </Col>
       </Row>
