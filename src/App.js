6727530832bd2383f7e08302eecd6d1d7614f886
Michelle Opey <mipanowicz@gmail.com>
2020-10-21 15:09:13 +0200

    new "homepage" combined

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import infobox from './components/InfoBox';
 import Sidebar from "./components/Sidebar";
 import Content from "./components/Content";
 import "./sass/style.scss";
-import Infobox from "./components/InfoBox";
 
 const App = () => {
   const [sidebarIsOpen, setSidebarOpen] = useState(true);
@@ -16,7 +14,6 @@ const App = () => {
         <Sidebar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
         <Content toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen} />
       </div>
-      <Route path="/infobox" component={Infobox}></Route>
     </Router>
   );
 };
