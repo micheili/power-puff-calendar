a0bcebc05b14bb2da00f5b8e2335343036cc5c66
snehal patel <snehal.kathiriya@gmail.com>
2020-10-21 21:55:01 +0200

    merged expressLogin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import infobox from './components/InfoBox';
 import Sidebar from "./components/Sidebar";
 import Content from "./components/Content";
 import "./sass/style.scss";
 import Infobox from "./components/InfoBox";
 
-const App = () => {
+// create and export the context
+export const Context = createContext();
+
+export default function App() {
+  const [contextVal, setContext] = useState({ currentUser: {} });
+
+  const updateContext = (updates) =>
+    setContext({
+      ...contextVal,
+      ...updates,
+    });
+
   const [sidebarIsOpen, setSidebarOpen] = useState(true);
   const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
+  //const [loggedInUser, setLoggedInUser] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const result = await (await fetch("/api/login")).json();
+      if (!result.error) {
+        updateContext({ currentUser: result });
+      } else {
+        updateContext({ currentUser: {} });
+        return;
+      }
+    })();
+  }, [contextVal]);
+
+  async function logout() {
+    const res = await fetch("/api/login", {
+      method: "DELETE",
+    });
+    const result = await res.json();
+    console.log(result);
+  }
 
   return (
-    <Router>
-      <div className="App wrapper">
-        <Sidebar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
-        <Content toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen} />
-      </div>
-      <Route path="/infobox" component={Infobox}></Route>
-    </Router>
+    <Context.Provider value={[contextVal, updateContext]}>
+      <Router>
+        <div className="App wrapper">
+          <Sidebar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
+          <Content
+            toggleSidebar={toggleSidebar}
+            sidebarIsOpen={sidebarIsOpen}
+            logout={logout}
+          />
+        </div>
+        <Route path="/infobox" component={Infobox}></Route>
+      </Router>
+    </Context.Provider>
   );
-};
-
-export default App;
+}
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import classNames from "classnames";
-import { Container } from "reactstrap";
-import { Switch, Route } from "react-router-dom";
+import { Container, Row } from "reactstrap";
+import { Switch, Route, Link } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import CalendarMonth from "../calendar/CalendarMonth";
@@ -9,31 +9,50 @@ import CalendarWeek from "../calendar/CalendarWeek";
 import CalendarDay from "../calendar/CalendarDay";
 import Invitation from "./Invitation";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
-import {
-    Button    
-  } from "reactstrap";
+import { Button } from "reactstrap";
+import { Context } from "../App";
 
+export default function Content({ sidebarIsOpen, toggleSidebar, logout }) {
+  const [context, updateContext] = useContext(Context);
 
-const Content = ({ sidebarIsOpen, toggleSidebar }) => (
-  <Container
-    fluid
-    className={classNames("content", { "is-open": sidebarIsOpen })}
-  >
-   <Button className="toogleSidebarButton" onClick={toggleSidebar}>
-   <FontAwesomeIcon  className="icon" icon={faBars} />
-    </Button>
-    <Switch>
-      <Route exact path="/" component={Login} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/calendar" component={CalendarMonth} />
-      <Route exact path="/calendarweek" component={CalendarWeek} />
-      <Route exact path="/calendarday" component={CalendarDay} />
-      <Route exact path="/invitation" component={Invitation} />
-    </Switch>
-  </Container>
-);
+  return (
+    <Container
+      fluid
+      className={classNames("content", { "is-open": sidebarIsOpen })}
+    >
+      <Row className="justify-content-between mb-3">
+        <Button className="toogleSidebarButton ml-4" onClick={toggleSidebar}>
+          <FontAwesomeIcon className="icon" icon={faBars} />
+        </Button>
+        {context.currentUser.firstName != undefined ? (
+          <Button
+            tag={Link}
+            to="/"
+            color="info"
+            className="text-light"
+            onClick={logout}
+          >
+            <div className="mt-2">
+              <FontAwesomeIcon className="mr-2" icon={faSignOutAlt} />
+              Logout
+            </div>
+          </Button>
+        ) : (
+          <></>
+        )}
+      </Row>
 
-export default Content;
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/calendar" component={CalendarMonth} />
+        <Route exact path="/calendarweek" component={CalendarWeek} />
+        <Route exact path="/calendarday" component={CalendarDay} />
+        <Route exact path="/invitation" component={Invitation} />
+      </Switch>
+    </Container>
+  );
+}
