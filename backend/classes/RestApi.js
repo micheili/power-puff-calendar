7945403a4784339f21ccd4e14a5f14c72e25989a
snehal patel <snehal.kathiriya@gmail.com>
2020-10-26 21:49:11 +0100

    small correction in rest api

--- backend/classes/RestApi.js
+++ backend/classes/RestApi.js
@@ -97,7 +97,7 @@ module.exports = class RestApi {
         /*sql*/ `
       SELECT e.* FROM Event e
       INNER JOIN Invite i ON e.id = i.eventId 
-      WHERE i.invitedUser = $userId AND accepted = ${req.query.accepted}
+      WHERE i.invitedUser = $userId AND accepted IS ${req.query.accepted}
       `,
         req.params
       );
