9c782bdebf3b19a27cf07cd7065ec02cd23d07d3
Michelle Opey <mipanowicz@gmail.com>
2020-10-29 09:51:06 +0100

    merge conflicts

--- src/components/Event.js
+++ src/components/Event.js
@@ -5,7 +5,7 @@ import {
   ButtonToggle,
   Button,
   CardText,
-  UncontrolledTooltip,
+  UncontrolledTooltip
 } from "reactstrap";
 import moment from "moment";
 import {
@@ -20,37 +20,18 @@ import {
   faTrashAlt,
   faUserPlus,
 } from "@fortawesome/free-solid-svg-icons";
-
 import { Context } from "../App";
 
 export default function Event(props) {
+  let {id, userId, title, description, start, stop } = props.myEvent;
   let [context, updateContext] = useContext(Context);
-  //let { id, title, eventCreator, description, start, stop } = props;
 
-  // if (invitedGuests === 0) {
-  //   showInviteButton()
-  // }
+  const loggedInUser = context.user.id;
 
-  // let loggedInUser;
-
-  // let hostName;
-  // if (eventCreator !== loggedInUser) {
-  //   hostName = <p>{eventCreator}</p>;
-  // }
-
-  //gör fetch från en view eller invite?
-  // async function fetchInvitees() {
-  //   const data = await fetch('api/invite' + id);  men eventId?
-  //   setInvitees(await data.json());
-
-  // if (invitedPeople > 0) {
-  //   //skapa en komponent med en lista
-  //   //på invited people som visar accepted-status
-  //   invitees = <InvitedList key="" />;
-  // } else {
-  //   invitees = null;
-  // }
-  let { id, title, description, start, stop } = props.myEvent;
+  if (!loggedInUser === userId) {
+    //hide edit and invite from event
+    console.log('hAAAAAAAAAj', loggedInUser, userId);
+  };
 
   let startMoment = moment(start);
   let stopMoment = moment(stop);
--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -5,7 +5,6 @@ import NewEvent from "./NewEvent";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { Context } from "../App";
-
 import {
   CardHeader,
   Card,
@@ -17,16 +16,6 @@ import {
 } from "reactstrap";
 
 const Infobox = (props) => {
-  // props = [
-  //   {
-  //     id: "",
-  //     eventTitle: "",
-  //     eventDescription: "",
-  //     start: "",
-  //     ends: "",
-  //     invitedGuests: "",
-  //   },
-  // ];
 
   const [context, updateContext] = useContext(Context);
 
@@ -34,38 +23,29 @@ const Infobox = (props) => {
   let date = "22/10";
   let year = "2020";
 
-  let eventDetails = null;
-  let eventList = null;
-  let defaultText = null;
-
   const addNewEvent = () => {
     updateContext({ showNewEvent: true });
   };
 
-  //visa detaljvy om bara ett event finns vald dag
-
-  if (myEvents.length === 1) {
-    eventDetails = <Event myEvent={myEvents[0]} />;
-  } //ifall det finns flera event
-  else if (myEvents.length > 1) {
-    //skapa en komponent för att visa en lista
-    //info för alla events eller id?
-    eventList = <EventList myEvents={myEvents} invitedEvents={invitedEvents} />;
-  } else {
-    defaultText = (
-      <CardText>
-        You have no events this day! <br></br>
-        Do you want to{" "}
-        <a
-          href="#"
-          onClick={addNewEvent}
-          className="text-dark font-weight-bolder link"
-        >
-          add a new event?
-        </a>
-      </CardText>
-    );
-  }
+  let eventDetails = (
+    <Event myEvent={myEvents[0]} invitedEvents={invitedEvents} />
+  );
+  let eventList = (
+    <EventList myEvents={myEvents} invitedEvents={invitedEvents} />
+  );
+  let defaultText = (
+    <CardText>
+      You have no events this day! <br></br>
+      Do you want to{" "}
+      <a
+        href="#"
+        onClick={addNewEvent}
+        className="text-dark font-weight-bolder link"
+      >
+        add a new event?
+      </a>
+    </CardText>
+  );
 
   //kanske formattera om date -> 10/7
   let dateText = (
