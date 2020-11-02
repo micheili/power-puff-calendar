6ba4a586e66677ec79fedc7401142cb4c1c3bc1d
snehal patel <snehal.kathiriya@gmail.com>
2020-11-02 16:13:15 +0100

    wip

--- src/components/ThemeChanger.js
+++ src/components/ThemeChanger.js
@@ -1,39 +1,44 @@
-import React, { useEffect } from 'react';
-import themeChanger from '../sass/_themeChanger.scss';
+import React, { useEffect, useContext } from "react";
+//import themeChanger from "../sass/_themeChanger.scss";
+import { Context } from "../App";
 
+export default function ThemeChanger() {
+  //const [colorTheme, setColorTheme] = useState('');
+  const [context, updateContext] = useContext(Context);
 
-
-export default function ThemeChanger ({colorTheme, setColorTheme}) {
-    const [colorTheme, setColorTheme] = useState('');
-
-    useEffect(() => {
-        const currentThemeColor = localStorage.getItem('theme-color');
-        if (currentThemeColor) {
-            setColorTheme(currentThemeColor);
-        }
-    }, []);
-
-    const handleClick = (theme) => {
-        setColorTheme(theme);
-        localStorage.setItem('theme-color', theme);
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem("theme-color");
+    if (currentThemeColor) {
+      updateContext({
+        colorTheme: currentThemeColor,
+      });
     }
+  }, []);
 
-    return(
-    
-        <div className='theme-options'>
-            <div id='theme-pink'
-                onClick={() => handleClick('theme-pink')}
-                className={`${colorTheme === 'theme-pink' ? 'active' : ''}`}
-            ></div>
-            <div id='theme-blue'
-                 onClick={() => handleClick('theme-blue')}
-                 className={`${colorTheme === 'theme-blue' ? 'active' : ''}`}
-            ></div>
-            <div id='theme-black'
-                 onClick={() => handleClick('theme-black')}
-                 className={`${colorTheme === 'theme-black' ? 'active' : ''}`}
-            ></div>
+  const handleClick = (theme) => {
+    updateContext({
+      colorTheme: theme,
+    });
+    localStorage.setItem("theme-color", theme);
+  };
 
-        </div>
-    );
+  return (
+    <div className="theme-options">
+      <div
+        id="theme-pink"
+        onClick={() => handleClick("theme-pink")}
+        className={`${context.colorTheme === "theme-pink" ? "active" : ""}`}
+      ></div>
+      <div
+        id="theme-blue"
+        onClick={() => handleClick("theme-blue")}
+        className={`${context.colorTheme === "theme-blue" ? "active" : ""}`}
+      ></div>
+      <div
+        id="theme-black"
+        onClick={() => handleClick("theme-black")}
+        className={`${context.colorTheme === "theme-black" ? "active" : ""}`}
+      ></div>
+    </div>
+  );
 }
